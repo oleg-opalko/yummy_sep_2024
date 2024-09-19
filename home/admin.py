16e998737a4f448d9d4635ea94f471d2bf68ca26
oleh.opalko <oopalko96@gmail.com>
2024-09-20 02:18:43 +0300

    Added changes lecture Django #3

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Dish
+from .models import Category, Dish, Event, Chef, ChefCategory, Gallery, Contact
 
 
 @admin.register(Category)
@@ -12,4 +12,41 @@ class CategoryAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
 
 
-admin.site.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ingredients', 'price', 'weight', 'photo', 'is_visible', 'sort', 'created_at', 'updated_at')
+    list_editable = ('name', 'ingredients', 'price', 'weight', 'photo', 'is_visible', 'sort')
+    list_filter = ('is_visible', 'created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+
+@admin.register(Event)
+class EventsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'sort', 'is_visible', 'description', 'photo', 'price', 'created_at', 'updated_at')
+    list_editable = ('sort', 'is_visible', 'description', 'photo', 'price')
+    list_filter = ('is_visible',)
+    date_hierarchy = 'created_at'
+
+@admin.register(ChefCategory)
+class ChefCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_visible', 'created_at', 'updated_at')
+    list_editable =  ('is_visible', )
+    list_filter = ('is_visible', )
+    date_hierarchy = 'created_at'
+
+@admin.register(Chef)
+class ChefAdmin(admin.ModelAdmin):
+    list_display = ('created_at' ,'first_name', 'last_name', 'photo', 'updated_at')
+    list_editable = ('first_name', 'last_name', 'photo')
+    date_hierarchy = 'created_at'
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'is_visible', 'photo', 'position', 'updated_at')
+    list_editable = ('is_visible', 'photo', 'position')
+    list_filter = ('is_visible', 'position')
+    date_hierarchy ='created_at'
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'address', 'phone', 'email', 'work_days', 'start_time', 'end_time', 'day_off', 'updated_at')
+    list_editable = ('address', 'phone', 'email', 'work_days', 'start_time', 'end_time', 'day_off')
+    date_hierarchy = 'created_at'
--- home/models.py
+++ home/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Category(models.Model):
     slug = models.SlugField(max_length=50, unique=True)
     name = models.CharField(max_length=50, unique=True)
@@ -21,16 +20,63 @@ class Dish(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     weight = models.IntegerField()
     unit = models.CharField(max_length=10)
-
     photo = models.ImageField(upload_to='dishes', blank=True, null=True)
-
     is_visible = models.BooleanField(default=True)
     sort = models.IntegerField(default=0)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    sort = models.IntegerField(default=0)
+    is_visible = models.BooleanField(default=True)
+    description = models.CharField(max_length=255, blank=True)
+    price = models.IntegerField(default=0)
+    photo = models.ImageField(upload_to='events', blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ChefCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    is_visible = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Chef(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    photo = models.ImageField(upload_to='chefs', blank=True, null=True)
+    chef_category = models.ForeignKey(ChefCategory, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Gallery(models.Model):
+    photo = models.ImageField(upload_to='gallery', blank=True, null=True)
+    position = models.IntegerField(default=0)
+    is_visible = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Contact(models.Model):
+    address = models.CharField(max_length=50)
+    phone = models.IntegerField(default=0)
+    email = models.CharField(max_length=50)
+    work_days = models.CharField(max_length=10)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    day_off = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

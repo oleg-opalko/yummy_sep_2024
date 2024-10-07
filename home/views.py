7ac9c2b5199a123c0701195a7c9bf9f6c63c8307
oleh.opalko <oopalko96@gmail.com>
2024-10-07 20:28:43 +0300

    Code refactoring

--- home/views.py
+++ home/views.py
@@ -1,26 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.dates import timezone_today
 
 from home.models import Category, Gallery, Chef, Event, Contact
 from home.templates.forms import ReservationForm
 
-
 # Create your views here.
-
-
 def index(request):
-
     if request.method == 'POST':
-        book_a_table_form = ReservationForm(request.POST)
-
-        if book_a_table_form.is_valid():
-            book_a_table_form.save()
-            return render(request, 'thanks.html')
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('thanks')
 
     categories = Category.objects.filter(is_visible=True).order_by('sort')
     gallery = Gallery.objects.filter(is_visible=True)
     chefs = Chef.objects.all()
-    events = Event.objects.filter(date_and_time__gte=timezone_today(), is_visible=True).order_by('date_and_time')
+    events = Event.objects.filter(date_and_time__gte=timezone_today(), is_visible=True)
     contacts = Contact.objects.all()
     book_a_table_form = ReservationForm()
 
@@ -34,3 +29,6 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+def thanks(request):
+    return render(request, 'thanks.html')
--- yummy_sep_2024/urls.py
+++ yummy_sep_2024/urls.py
@@ -18,12 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from home.views import index
+from home.views import index, thanks
 from yummy_sep_2024 import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', index, name='index'),
+    path('thanks/', thanks, name='thanks'),
 ]
 
 if settings.DEBUG:
